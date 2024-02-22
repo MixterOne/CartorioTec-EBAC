67d3f07b97b29589fa326fb4fba5f0b867745d7d
Matheus_R_I <84294599+MixterOne@users.noreply.github.com>
2024-02-22 10:32:46 -0300

    Validação por senha
    Foi implementado uma validação por senha, onde o cliente tem acesso ao conteúdo quando digita "admin"

--- Projeto1.c
+++ Projeto1.c
@@ -112,57 +112,68 @@ int deletar()
 	
 }
 
-int main()
-{
+int main(){
+	setlocale(LC_ALL,"Portuguese");
+	
 	int opcao = 0; //Definindo as variáveis
 	int laco = 1;
+	char senhadigitada[10]="a";
+	int comparacao;
 	
+	printf("### Cartório Tec ### \n\n");
+	printf("Login de administrador!\n\nDigite a sua senha:");
+	scanf("%s", senhadigitada);
 	
-	for(laco=1; laco=1;)
-	{
-		system("cls");
+	comparacao = strcmp(senhadigitada, "admin");
+	
+	if(comparacao == 0){
+		for(laco=1; laco=1;){
+			system("cls");
 		
-		setlocale(LC_ALL, "Portuguese"); //Definindo a linguagem
+			setlocale(LC_ALL, "Portuguese"); //Definindo a linguagem
 	
-		printf("### Cartório Tec ### \n\n"); //Inicio do menu
-		printf("Escolha a opção desejada do menu: \n\n");
-		printf("\t1 - Registrar nomes \n");
-		printf("\t2 - Consultar nomes \n");
-		printf("\t3 - Deletar nomes \n");
-		printf("\t4 - Sair do sistema \n\n");
-		printf("Opção que deseja: ");//Final do menu
+			printf("### Cartório Tec ### \n\n"); //Inicio do menu
+			printf("Escolha a opção desejada do menu: \n\n");
+			printf("\t1 - Registrar nomes \n");
+			printf("\t2 - Consultar nomes \n");
+			printf("\t3 - Deletar nomes \n");
+			printf("\t4 - Sair do sistema \n\n");
+			printf("Opção que deseja: ");//Final do menu
 	
-		scanf("%d", &opcao); //Armazenando a escolha do usuário
+			scanf("%d", &opcao); //Armazenando a escolha do usuário
 	
-		system("cls"); //responsavel por limpar a tela
+			system("cls"); //responsavel por limpar a tela
 		
 		
-		switch(opcao) //Inicio da seleção do menu
-		{
-			case 1:
-				registro(); //chamada de funções
-			break;
+			switch(opcao) //Inicio da seleção do menu
+			{
+				case 1:
+					registro(); //chamada de funções
+				break;
 			
-			case 2:
-				consulta(); //chamada de funções
-			break;
+				case 2:
+					consulta(); //chamada de funções
+				break;
 			
-			case 3:
-				deletar(); //chamada de funções
-			break;	
+				case 3:
+					deletar(); //chamada de funções
+				break;	
 			
-			case 4:
-				printf("Obrigado por utilizar o sistema!\n");
-				return 0;
-			break;	
+				case 4:
+					printf("Obrigado por utilizar o sistema!\n");
+					return 0;
+				break;	
 			
-			default:
-				printf("Essa opção não está disponivel!\n");
-				system("pause");
-			break;
-		} //Fim da seleção
+				default:
+					printf("Essa opção não está disponivel!\n");
+					system("pause");
+				break;
+			} //Fim da seleção
 	
+		}
 	}
+	else
+		printf("Senha incorreta!");
 }
 
 
